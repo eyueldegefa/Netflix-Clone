5fc475ff04bd6553f04a03c85e74118ad385263e
Eyuel Ergudo <eyuel144@gmail.com>
2024-08-04 21:02:55 +0300

    Header and Footer

--- netflix-clone/src/Components/Footer/Footer.js
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import './Footer.css'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-wrapper'>
+      <div>
+        <ul className='footerlists footer-wrapper'>
+          <li><FacebookOutlinedIcon /></li>
+          <li><InstagramIcon /></li>
+          <li><YouTubeIcon /></li>
+        </ul>
+        <ul className='footerlists'>
+          <li>Audio Description</li>
+          <li>Investor Relation</li>
+          <li>Legal Notice</li>
+          <li className='secret-code'>Service Code</li>
+          <li className='copy-right'>&copy; 1997-2024 Netflix, Inc.</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className='footerlists'>
+          <li>Help Center</li>
+          <li>Jobs</li>
+          <li>Cookie Preferences</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className='footerlists'>
+          <li>Gift Cards</li>
+          <li>Terms of Use</li>
+          <li>Corporate Information</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className='footerlists'>
+          <li>Media Center</li>
+          <li>Privacy</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- netflix-clone/src/Components/Header/Header.js
+++ netflix-clone/src/Components/Header/Header.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import './Header.css'
+import Logo from '../../assets/images/Logo.png'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header-Wrapper'>
+      <div className='Leftside'>
+        <ul className='headerlists'>
+            <li className='nflogo'><img src={Logo} alt="Netflix Logo" width="100"/></li>
+            <li className='gvspace'>Netflix</li>
+            <li className='gvspace'>Home</li>
+            <li className='gvspace'>TVShows</li>
+            <li className='gvspace'>Movies</li>
+            <li className='gvspace'>Latest</li>
+            <li className='gvspace'>MyList</li>
+            <li className='gvspace'>Browse by languages</li>
+        </ul>
+      </div>
+
+      <div className='Rightside'>
+        <ul className='headerlists'>
+          <li className='gvspace'><SearchIcon /></li>
+          <li className='gvspace'><NotificationsIcon /></li>
+          <li className='gvspace'><AccountBoxIcon /></li>
+          <li className='gvspace'><ArrowDropDownIcon /></li>
+        </ul>
+      </div>  
+    </div>
   )
 }
 
